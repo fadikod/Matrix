b97aab658b58878fd4f1e4d3c47423b23dcccaa4
fadikod <103376120+fadikod@users.noreply.github.com>
2026-02-02 22:24:37 +0100

    Add files via upload

---
+++ Monday-JS-Basic/script.js
@@ -0,0 +1,72 @@
+// for(let i=1;i<=135;i++){
+//     console.log(i)
+// }
+
+//2
+
+// for(let i=1; i<=135; i++){
+
+//     if(i % 2 !==0){console.log(i)}
+// }
+
+//3
+// let Sum=0;
+// let Number =0;
+// console.log("Number is: " + Number + " Sum is:" + Sum )
+// Number++;
+// for(let i=1; i<=135; i++)
+// {
+//     Sum+=Number;
+//     console.log("Number is: " + Number + " Sum is:" + Sum )
+//     Number++;
+
+// }
+
+// //4
+// let x=[1,4,2,12,15]
+// for(i=0; i<x.length; i++){
+//     console.log(x[i])
+// }
+
+
+
+// //5
+// let array=[5,-3,4,0,18,6,-7,30]
+// let max=array[0]
+// for(i=0; i<array.length; i++){
+//     if(array[i]>max){max=array[i]}
+// }
+// console.log(max)
+
+
+// //6
+// let sum=0
+// let array=[1,4,2,12,15]
+// for(i=0; i<array.length; i++){
+//     sum+=array[i]
+// }
+
+// console.log(sum/array.length)
+
+// //7
+// let array= [2,-1,4,-3]
+// for(let i=0; i<array.length; i++){
+//     if(array[i]<0){
+//         array[i]=0
+//     }
+
+// }
+//     console.log(array)
+
+
+
+//8
+
+let array= [2,-1,4,-3]
+for(let i=0; i<array.length; i++){
+    if(array[i]<0){
+        array[i]="Turing"
+    }
+
+}
+    console.log(array)
