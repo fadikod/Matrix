8fe41c169317bc3c8d50fade894de84759165e1d
fadikod <103376120+fadikod@users.noreply.github.com>
2026-02-09 16:08:41 +0100

    Add files via upload

---
+++ Exercises1-advanced JS/script.js
@@ -0,0 +1,52 @@
+// let { name, years: age, isAdmin = false } = user;
+
+
+// let ourPlanetName = "Earth";
+// let currentVisitorName = "John";
+
+
+
+// This code will throw an error
+
+
+
+// let user={}
+//     user.name="John";
+//     user.surname="Smith";
+//     user.nameme="Pete";
+//     delete user.name;
+
+
+
+
+// const protects the variable, not the object’s contents.
+
+
+
+
+// let salaries = {
+//   Fred: 100,
+//   Ted: 160,
+//   Ghaith: 130
+// };
+
+// let sum = 0;
+
+// for (let key in salaries) {
+//   sum += salaries[key];
+// }
+
+// console.log(sum);
+
+
+
+// result = (a + b < 4) ? 'Below' : 'Over';
+
+
+
+
+// message =
+//   (login == 'Employee') ? 'Hello' :
+//   (login == 'Director') ? 'Greetings' :
+//   (login == '') ? 'No login' :
+//   '';
